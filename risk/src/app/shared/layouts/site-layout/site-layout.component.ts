7d025427d6e92d432cfd1644ebaaa6174c5de350
rat6666 <itpolalex@gmail.com>
2020-05-24 15:47:52 +0300

    feat: styles ref

--- risk/src/app/shared/layouts/site-layout/site-layout.component.ts
+++ risk/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
-import { sessionStorageKey } from '../../enums';
+import { sessionStorageKey, defaultRisk } from '../../enums';
+import { SelectedRiskService } from '../../services/selected-risk.service';
 
 @Component({
   selector: 'app-site-layout',
@@ -9,7 +10,11 @@ import { sessionStorageKey } from '../../enums';
   styleUrls: ['./site-layout.component.scss'],
 })
 export class SiteLayoutComponent {
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private selectedRiskService: SelectedRiskService
+  ) {}
 
   images: any[];
   public userName: string = sessionStorage.getItem(sessionStorageKey.userName);
@@ -18,11 +23,13 @@ export class SiteLayoutComponent {
 
   public onManagePage(): void {
     this.router.navigate(['manage']);
+    this.selectedRiskService.selectRisk(defaultRisk);
     this.isMainPage = !this.isMainPage;
   }
 
   public onMainPage(): void {
     this.router.navigate(['main']);
+    this.selectedRiskService.selectRisk(defaultRisk);
     this.isMainPage = !this.isMainPage;
   }
 
