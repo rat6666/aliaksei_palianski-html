942bfcedd9e88728ddd31cff91ee5f8b27ec12b0
rat6666 <itpolalex@gmail.com>
2020-05-24 17:39:50 +0300

    feat: disabling edit component implemented

--- risk/src/app/main-page/main-page.component.ts
+++ risk/src/app/main-page/main-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectedRiskService } from '../shared/services/selected-risk.service';
+import { Risk } from '../shared/interfaces';
+import { defaultRisk } from '../shared/enums';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent {}
+export class MainPageComponent implements OnInit {
+  constructor(private getSelectedRisk: SelectedRiskService) {}
+
+  risk: Risk = defaultRisk;
+  ngOnInit() {
+    this.getSelectedRisk.selectedRisk$.subscribe((data) => {
+      this.risk = data;
+    });
+  }
+}
--- risk/src/app/manage-page/manage-page.component.ts
+++ risk/src/app/manage-page/manage-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SelectedRiskService } from '../shared/services/selected-risk.service';
+import { defaultRisk } from '../shared/enums';
+import { Risk } from '../shared/interfaces';
 
 @Component({
   selector: 'app-manage-page',
   templateUrl: './manage-page.component.html',
   styleUrls: ['./manage-page.component.scss'],
 })
-export class ManagePageComponent {}
+export class ManagePageComponent implements OnInit {
+  constructor(private getSelectedRisk: SelectedRiskService) {}
+
+  risk: Risk = defaultRisk;
+  ngOnInit() {
+    this.getSelectedRisk.selectedRisk$.subscribe((data) => {
+      this.risk = data;
+    });
+  }
+}
