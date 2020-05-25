b8d725d6c8df73ce220e9d82487472b7ab6991d0
rat6666 <itpolalex@gmail.com>
2020-05-25 09:11:57 +0300

    feat: ref

--- risk/src/app/shared/enums.ts
+++ risk/src/app/shared/enums.ts
@@ -50,37 +50,3 @@ export const defaultAddRisk = {
   probability: null,
   description: null,
 };
-
-export const MOCK = `[
-  {
-  "riskName": "Some data here numder 1",
-  "time": "1",
-  "prob": "1",
-  "description": "this is data numder 1 big desription here dfgd dfg dfg ddfg dfdg dfdg dfgd dfg "
-},
-  {
-  "riskName": "Some data here numder 2",
-  "time": "2",
-  "prob": "2",
-  "description": "this is data numder 2"
-},
-  {
-  "riskName": "Some data here numder 3",
-  "time": "3",
-  "prob": "3",
-  "description": "this is data numder 3"
-},
-  {
-  "riskName": "Some data here numder 4",
-  "time": "4",
-  "prob": "4",
-  "description": "this is data numder 4"
-},
-{
-  "riskName": "INCREDIBLE TOOLING from ANGULAR desription",
-  "time": "1",
-  "prob": "1",
-  "description": "Build features quickly with simple, declarative templates. Extend the template language with your own components and use a wide array of existing components. Get immediate Angular-specific help and feedback with nearly every IDE and editor. All this comes together so you can focus on building amazing apps rather than trying to make the code work."
-}
-
-],`;
